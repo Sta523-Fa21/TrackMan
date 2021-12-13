7fc8e6cc16e3ef17414ebace96f113dd85c875c6
Matt Dockman <matthew.dockman@duke.edu>
2021-12-13 16:27:43 -0500

    Final

---
+++ R/shiny_server.R
@@ -0,0 +1,18 @@
+#'@name shiny_server
+#'
+#'@title Shiny Server
+#'
+#'@description Provides the server component of a Shiny app implementation
+#'
+#'@param input
+#'@param output
+#'@param session
+#'
+#'@return Yields the server portion of the Shiny app implementation of our predictive modeling exercise
+#'
+#'
+#'
+#'@export
+
+library(shiny)
+
---
+++ R/shiny_user_interface.R
@@ -0,0 +1,11 @@
+#'@name shiny_user_interface
+#'
+#'@title Shiny User Interface
+#'
+#'@description Provides the user interface component of our Shiny app implementation of our predictive modeling exercise.
+#'
+#'@param
+#'
+#'
+#'
+#'
